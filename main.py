59eba954d1719bc8015757c32bfe247c9a3c20dc
James Joynson-Ellis <jamestravisje@gmail.com>
2023-02-08 13:22:11 +0000

    Added a change to the code to allow for better optimisation for the future album support.

--- main.py
+++ main.py
@@ -42,44 +42,74 @@ def get_html(website):
     return html_content
 
 
+def get_image(html_contents):
+    """
+    Gets the bytes for the image file.
+    :param html_contents:
+    :return: bytes
+    """
+
+    popup_images = []
+    for element in html_contents:
+        if "<a class=\"popupImage\"" in element:
+            popup_images.append(element)
+    image_link = popup_images[0].replace("<a class=\"popupImage\" href=\"", "").replace("\">", "").replace(" ", "")
+
+    return requests.get(image_link).content
+
+
+def get_song_file(html_contents):
+    """
+    Gets the binary for the mp3 files.
+    :param html_contents:
+    :return: list
+    """
+
+    links = []
+    content = []
+    for element in html_contents:
+        if "https://t4.bcbits.com" in element:
+            temp_list = element.split("https://")
+            for item in temp_list:
+                if "t4.bcbits.com" in item:
+                    links.append("https://" + item.replace("}", ""))
+
+    for link in links:
+        content.append(requests.get(link).content)
+
+    return content
+
+
 def get_files(dict_links, names_of_songs, save_folder):
     """
     Gets the files for the
     :param dict_links: dict
     :param names_of_songs: list
     :param save_folder: string
-    :return:
+    :return: null
     """
+
+    # TODO: Split up the creation of files into seperate commands, to save space and simplicity.
+    # TODO: Add in support for album accounts.
 
     for name in names_of_songs:
         song_name = name
         song_link = dict_links.get(song_name)
 
         html_contents = get_html(song_link).split("\n")
-        popup_images = []
-        for element in html_contents:
-            if "<a class=\"popupImage\"" in element:
-                popup_images.append(element)
-        image_link = popup_images[0].replace("<a class=\"popupImage\" href=\"", "").replace("\">", "").replace(" ", "")
-        print("Image " + f"{save_folder}/{song_name}.jpg" + " has been saved.")
 
+        image_content = get_image(html_contents)
         with open(f"{save_folder}/{song_name}.jpg", "wb") as file:
-            file.write(requests.get(image_link).content)
+            file.write(image_content)
+            print("Image " + f"{save_folder}/{song_name}.jpg" + " has been saved.")
             file.close()
-        # TODO: Trigger the button to download the music on these pages.
 
-        links = []
-        for element in html_contents:
-            if "https://t4.bcbits.com" in element:
-                temp_list = element.split("https://")
-                for item in temp_list:
-                    if "t4.bcbits.com" in item:
-                        links.append("https://" + item.replace("}", ""))
-
-        with open(f"{save_folder}/{song_name}.mp3", "wb") as file:
-            file.write(requests.get(links[0]).content)
-            file.close()
-        print("Song " + f"{save_folder}/{song_name}.mp3" + f" has been saved with link {links[0]}. \n")
+        song_bytes = get_song_file(html_contents)
+        for binary in song_bytes:
+            with open(f"{save_folder}/{song_name}.mp3", "wb") as file:
+                file.write(binary)
+                print("Song " + f"{save_folder}/{song_name}.mp3 has been saved. \n")
+                file.close()
 
 
 def main():
@@ -91,7 +121,7 @@ def main():
             links[links.index(item)] = item.replace("\n", "")
         n_links = []
         for link in links:
-            if not "/album/" in link:
+            if "/album/" not in link:
                 n_links.append(link)
         file.close()
     dict_links, names_of_songs = create_dictionary(n_links)
